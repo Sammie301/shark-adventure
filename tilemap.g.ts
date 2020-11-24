4e77da4ae50abcad4fa6faca5e2e4a3444935ff9
Samuel Bonner <74755543+Sammie301@users.noreply.github.com>
2020-11-24 05:26:19 -0800

    Initial files for MakeCode project

---
+++ tilemap.g.ts
@@ -0,0 +1,31 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers.registerTilemapFactory(function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level": return tiles.createTilemap(hex`10001000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000e000f00100000000000000000000000090b0b0b0b0a00000000000000000000080c0c0c0c0d0000000000000000000005070707070600000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000011001100120013000e000f00100011000404040404040404040404040404040402030303030303030303030303030301`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . 2 2 2 2 2 2 . . . . . 
+. . . . . 2 2 2 2 2 2 . . . . . 
+. . . . . 2 2 2 2 2 2 . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.builtin.oceanDepths10,sprites.builtin.oceanDepths8,sprites.builtin.oceanDepths9,sprites.builtin.oceanDepths4,sprites.builtin.oceanSand13,sprites.builtin.oceanSand15,sprites.builtin.oceanSand14,sprites.builtin.oceanSand8,sprites.builtin.oceanSand5,sprites.builtin.oceanSand7,sprites.builtin.oceanSand6,sprites.builtin.oceanSand9,sprites.builtin.oceanSand11,sprites.builtin.coral0,sprites.builtin.coral1,sprites.builtin.coral2,sprites.builtin.coral3,sprites.builtin.coral4,sprites.builtin.coral5], TileScale.Sixteen)
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
